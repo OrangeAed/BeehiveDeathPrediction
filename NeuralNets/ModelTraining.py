db40ca981bc43389ebec679751bfdbfb0b34f585
OrangeAed <ajsimonsrudolph@gmail.com>
2025-02-20 11:43:45 -0500

    deleted bad attempt at neural network

--- NeuralNets/ModelTraining.py
+++
@@ -1,6 +0,0 @@
-class ModelTraining:
-    def __init__(self, model):
-        self.model = model
-
-    def train_model(self, X_train, y_train, epochs=20, batch_size=32):
-        self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
